c90d306818144077f245c6606d7ebfb5206b9b24
Ramon <r4mon.guimaraes@gmail.com>
2022-05-24 19:24:29 -0300

    feature: Adicionando login/cadastro através do Google

--- app/src/main/java/com/ramonguimaraes/horacerta/ui/authentication/mainLogin/LoginFragment.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/ui/authentication/mainLogin/LoginFragment.kt
@@ -1,18 +1,99 @@
 package com.ramonguimaraes.horacerta.ui.authentication.mainLogin
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import com.ramonguimaraes.horacerta.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.ramonguimaraes.horacerta.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentLoginBinding
+    private lateinit var activityLaucher: ActivityResultLauncher<Intent>
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        mBinding = FragmentLoginBinding.inflate(inflater)
+
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(KEY)
+            .requestEmail()
+            .build()
+
+        activity?.let { googleSignInClient = GoogleSignIn.getClient(it, options) }
+
+        registrationForResultGoogleSignIn()
+        configButtonsOnClick()
+
+        return mBinding.root
+    }
+
+    private fun registrationForResultGoogleSignIn() {
+        activityLaucher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                    try {
+                        val conta = task.result
+                        conta.idToken?.let { googleLogin(it) }
+                    } catch (e: Exception) {
+                        Log.e("erro", e.message.toString())
+                    }
+                }
+            }
+    }
+
+    private fun signIn(googleSignInClient: GoogleSignInClient) {
+        val intent = googleSignInClient.signInIntent
+
+        activityLaucher.launch(intent)
+    }
+
+    private fun signOut(googleSignInClient: GoogleSignInClient) {
+        FirebaseAuth.getInstance().signOut()
+        googleSignInClient.signOut().addOnCompleteListener {
+            if (it.isSuccessful) {
+                activity?.finish()
+            }
+        }
+    }
+
+    private fun googleLogin(token: String) {
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(context, "Deu certo", Toast.LENGTH_SHORT).show()
+            } else {
+                signOut(googleSignInClient)
+                Toast.makeText(context, "Deu errado", Toast.LENGTH_SHORT).show()
+                Log.d("error", it.exception.toString())
+            }
+        }
+    }
+
+    private fun configButtonsOnClick() {
+        mBinding.buttonGoogleAuth.setOnClickListener {
+            signIn(googleSignInClient)
+        }
+    }
+
+    companion object {
+        private const val KEY =
+            "560084543445-sjcsmqjhamao63o4l7jiprbv6k49ddi8.apps.googleusercontent.com"
     }
 }
