90972eefd750bf5920cfe7734c04f4ff4d72d384
Ramon <r4mon.guimaraes@gmail.com>
2022-05-23 21:11:57 -0300

    criando pacote ui

--- app/src/main/java/com/ramonguimaraes/horacerta/ui/MainActivity.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ramonguimaraes.horacerta
+package com.ramonguimaraes.horacerta.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ramonguimaraes.horacerta.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
