b024386ad4f10c0134218899e4541044b3ffd3d1
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-04-21 17:43:24 -0300

    #7 feat add navigation login to create account

--- app/src/main/java/com/ramonguimaraes/horacerta/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/ui/authentication/LoginFragment.kt
@@ -1,14 +1,17 @@
 package com.ramonguimaraes.horacerta.ui.authentication
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.ramonguimaraes.horacerta.databinding.FragmentLoginBinding
 import com.ramonguimaraes.horacerta.domain.resource.Resource
 import com.ramonguimaraes.horacerta.presenter.authentication.LoginViewModel
+import com.ramonguimaraes.horacerta.utils.AccountType
 import com.ramonguimaraes.horacerta.utils.gone
 import com.ramonguimaraes.horacerta.utils.hideKeyboard
 import com.ramonguimaraes.horacerta.utils.visible
@@ -30,6 +33,9 @@ class LoginFragment : Fragment() {
         binding.btnLogin.setOnClickListener {
             it.hideKeyboard()
             viewModel.login()
+        }
+        binding.txtCreateAccountLink.setOnClickListener {
+            setAccountTypeDialog()
         }
         setObserver()
         return binding.root
@@ -59,7 +65,30 @@ class LoginFragment : Fragment() {
     private fun hideLoading() {
         binding.progressBar.gone()
     }
+
     private fun moveToHome() {}
+
+    private fun setAccountTypeDialog() {
+        val builder = AlertDialog.Builder(context)
+        builder.apply {
+            setMessage("Você deseja criar uma conta para sua empresa ou uma conta de cliente?")
+            setPositiveButton("Cliente") { dialog, _ ->
+                moveToCreateAccount(AccountType.CLIENT)
+                dialog.dismiss()
+            }
+            setNegativeButton("Empresa") { dialog, _ ->
+                moveToCreateAccount(AccountType.COMPANY)
+                dialog.dismiss()
+            }
+        }
+        builder.show()
+    }
+
+    private fun moveToCreateAccount(accountType: AccountType) {
+        val action = LoginFragmentDirections.actionLoginFragmentToCreateAccountFragment(accountType)
+        findNavController().navigate(action)
+    }
+
     private fun showSuccess() {
         Toast.makeText(
             context,
@@ -67,6 +96,7 @@ class LoginFragment : Fragment() {
             Toast.LENGTH_SHORT
         ).show()
     }
+
     private fun showError() {
         Toast.makeText(context, "Login falhou", Toast.LENGTH_SHORT)
             .show()
---
+++ app/src/main/java/com/ramonguimaraes/horacerta/utils/AccountType.kt
@@ -0,0 +1,6 @@
+package com.ramonguimaraes.horacerta.utils
+
+enum class AccountType {
+    CLIENT,
+    COMPANY
+}
