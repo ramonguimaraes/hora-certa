c5d4f00b80a47a24aba9ead7a39ed2e2c3de23c8
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-09-12 18:42:32 -0300

    save schedule config

--- app/src/main/java/com/ramonguimaraes/horacerta/data/scheduleConfig/ScheduleConfigRepositoryImpl.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/data/scheduleConfig/ScheduleConfigRepositoryImpl.kt
@@ -1,14 +1,26 @@
 package com.ramonguimaraes.horacerta.data.scheduleConfig
 
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.ramonguimaraes.horacerta.domain.resource.Resource
 import com.ramonguimaraes.horacerta.domain.scheduleConfig.model.ScheduleConfig
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.model.toHashMap
 import com.ramonguimaraes.horacerta.domain.scheduleConfig.repository.ScheduleConfigRepository
+import com.ramonguimaraes.horacerta.utils.DayOfWeek
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+import java.time.LocalTime
 
-class ScheduleConfigRepositoryImpl(private val db: FirebaseFirestore): ScheduleConfigRepository {
+class ScheduleConfigRepositoryImpl(private val db: FirebaseFirestore) : ScheduleConfigRepository {
 
     override suspend fun save(scheduleConfig: ScheduleConfig): Resource<ScheduleConfig> {
-        TODO("Not yet implemented")
+        return try {
+            db.collection("ScheduleConfig").add(scheduleConfig.toHashMap())
+            Resource.Success(scheduleConfig)
+        } catch (e: Exception) {
+            Log.e("ScheduleConfigRepositoryImpl", e.message.toString())
+            Resource.Failure(e)
+        }
     }
 
     override suspend fun delete(id: String): Resource<Boolean> {
