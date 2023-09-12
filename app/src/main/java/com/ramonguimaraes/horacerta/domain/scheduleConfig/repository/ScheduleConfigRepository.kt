f37b4043f2c325995138adcedfcdee41f6f5ece0
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-09-11 21:51:02 -0300

    add schedule config repository interface

---
+++ app/src/main/java/com/ramonguimaraes/horacerta/domain/scheduleConfig/repository/ScheduleConfigRepository.kt
@@ -0,0 +1,10 @@
+package com.ramonguimaraes.horacerta.domain.scheduleConfig.repository
+
+import com.ramonguimaraes.horacerta.domain.resource.Resource
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.model.ScheduleConfig
+
+interface ScheduleConfigRepository {
+    suspend fun save(scheduleConfig: ScheduleConfig): Resource<ScheduleConfig>
+    suspend fun delete(id: String): Resource<Boolean>
+    suspend fun list(): Resource<List<ScheduleConfig>>
+}
