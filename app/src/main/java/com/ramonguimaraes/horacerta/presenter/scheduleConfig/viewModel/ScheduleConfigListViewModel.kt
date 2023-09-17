297f5a4adae9ca8f31c0999503e88a47c119758e
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-09-16 23:23:57 -0300

    list scheduleConfigs

---
+++ app/src/main/java/com/ramonguimaraes/horacerta/presenter/scheduleConfig/viewModel/ScheduleConfigListViewModel.kt
@@ -0,0 +1,27 @@
+package com.ramonguimaraes.horacerta.presenter.scheduleConfig.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ramonguimaraes.horacerta.domain.resource.Resource
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.model.ScheduleConfig
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.useCase.ScheduleConfigListUseCase
+import kotlinx.coroutines.launch
+
+class ScheduleConfigListViewModel(
+    private val scheduleConfigListUseCase: ScheduleConfigListUseCase
+) : ViewModel() {
+
+    private val mScheduleConfigList = MutableLiveData<Resource<List<ScheduleConfig>>>()
+    val scheduleConfigList: LiveData<Resource<List<ScheduleConfig>>> get() = mScheduleConfigList
+
+    init {
+        loadScheduleConfigList()
+    }
+
+    fun loadScheduleConfigList() {
+        mScheduleConfigList.postValue(Resource.Loading)
+        viewModelScope.launch { mScheduleConfigList.postValue(scheduleConfigListUseCase.invoke()) }
+    }
+}
