985c0995df15773080b51e16e5b398d2e9e6fe74
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-09-27 11:55:00 -0300

    fix schedule available hors generation

--- app/src/main/java/com/ramonguimaraes/horacerta/domain/schedule/useCase/GetAvailableHorsUseCase.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/domain/schedule/useCase/GetAvailableHorsUseCase.kt
@@ -1,15 +1,25 @@
 package com.ramonguimaraes.horacerta.domain.schedule.useCase
 
+import com.ramonguimaraes.horacerta.domain.resource.Resource
 import com.ramonguimaraes.horacerta.domain.schedule.model.ScheduledTime
 import com.ramonguimaraes.horacerta.domain.schedule.model.TimeInterval
 import com.ramonguimaraes.horacerta.domain.schedule.repository.ScheduleRepository
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.model.ScheduleConfig
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.repository.ScheduleConfigRepository
+import com.ramonguimaraes.horacerta.domain.scheduleConfig.useCase.ScheduleConfigListUseCase
+import com.ramonguimaraes.horacerta.domain.services.userCase.LoadServicesUseCase
+import com.ramonguimaraes.horacerta.utils.DayOfWeek
 import com.ramonguimaraes.horacerta.utils.toLocalTime
 import java.time.LocalTime
 import java.util.Calendar
 
-class GetAvailableHorsUseCase(private val repository: ScheduleRepository) {
+class GetAvailableHorsUseCase(
+    private val repository: ScheduleRepository,
+    private val scheduleConfigRepository: ScheduleConfigRepository
+) {
 
-    suspend fun getAvailableHors(calendar: Calendar): List<TimeInterval> {
+    suspend fun getAvailableHors(calendar: Calendar, companyUid: String = "cY2YnxL8bBPLGj0GAjU8MTqFx213"): List<TimeInterval> {
+        val scheduleConfig = getScheduleConfig(calendar, companyUid) ?: return listOf()
 
         calendar.set(Calendar.MINUTE, 0)
         calendar.set(Calendar.SECOND, 0)
@@ -19,8 +29,10 @@ class GetAvailableHorsUseCase(private val repository: ScheduleRepository) {
             result.addAll(it)
         }
         val lista = geraLista(
-            LocalTime.of(8, 0),
-            LocalTime.of(18, 0),
+            scheduleConfig.openTime,
+            scheduleConfig.closeTime,
+            scheduleConfig.intervalStart,
+            scheduleConfig.intervalEnd,
             calendar
         )
         result.forEach { scheduled ->
@@ -38,13 +50,40 @@ class GetAvailableHorsUseCase(private val repository: ScheduleRepository) {
         return lista
     }
 
+    private suspend fun getScheduleConfig(calendar: Calendar, companyUid: String): ScheduleConfig? {
+        var scheduleConfig: ScheduleConfig? = null
+        val result = scheduleConfigRepository.getByDay(
+            getDayOfWeek(calendar.get(Calendar.DAY_OF_WEEK)),
+            companyUid
+        )
+        result.mapResourceSuccess {
+            scheduleConfig = it
+        }
+        return scheduleConfig
+    }
+
+    private fun getDayOfWeek(day: Int): DayOfWeek {
+        return when (day) {
+            Calendar.SUNDAY -> DayOfWeek.SUNDAY
+            Calendar.MONDAY -> DayOfWeek.MONDAY
+            Calendar.TUESDAY -> DayOfWeek.TUESDAY
+            Calendar.WEDNESDAY -> DayOfWeek.WEDNESDAY
+            Calendar.THURSDAY -> DayOfWeek.THURSDAY
+            Calendar.FRIDAY -> DayOfWeek.FRIDAY
+            else -> DayOfWeek.SATURDAY
+        }
+    }
+
     private fun geraLista(
         inicio: LocalTime,
         fim: LocalTime,
+        intervalStart: LocalTime,
+        intervalEnd: LocalTime,
         calendar: Calendar
     ): MutableList<TimeInterval> {
         val now = Calendar.getInstance()
         var horario = inicio
+        var interval = intervalStart
         val list = mutableListOf<TimeInterval>()
 
         if (now.time.before(calendar.time)) {
@@ -58,6 +97,13 @@ class GetAvailableHorsUseCase(private val repository: ScheduleRepository) {
         while (horario.isBefore(fim)) {
             list.add(TimeInterval(horario))
             horario = horario.plusMinutes(INTERVAL)
+        }
+
+        while (interval.isBefore(intervalEnd)) {
+            list.removeIf {
+                it.time == TimeInterval(interval).time
+            }
+            interval = interval.plusMinutes(INTERVAL)
         }
 
         return list
