657516fc37d7f86a0d9cec3c29945916b4a05211
ramonguimaraes <r4mon.guimaraes@gmail.com>
2023-10-07 19:41:07 -0300

    fix (Home): fix home backstack crash

--- app/src/main/java/com/ramonguimaraes/horacerta/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ramonguimaraes/horacerta/ui/home/HomeFragment.kt
@@ -1,25 +1,15 @@
 package com.ramonguimaraes.horacerta.ui.home
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ViewSwitcher
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.NavigationUiSaveStateControl
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ramonguimaraes.horacerta.R
-import com.ramonguimaraes.horacerta.domain.companyProfile.model.CompanyProfile
-import com.ramonguimaraes.horacerta.domain.resource.Resource
-import com.ramonguimaraes.horacerta.presenter.companyProfile.model.CompanyProfileView
-import com.ramonguimaraes.horacerta.presenter.home.HomeViewModel
-import com.ramonguimaraes.horacerta.ui.companyProfile.CompanyProfileFragment
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
 
@@ -30,6 +20,7 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    @OptIn(NavigationUiSaveStateControl::class)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -40,6 +31,13 @@ class HomeFragment : Fragment() {
 
         if (navController != null) {
             NavigationUI.setupWithNavController(navView, navController, false)
+        }
+
+        navView.setOnItemSelectedListener {
+            if (childFragmentManager.backStackEntryCount > 0) {
+                childFragmentManager.popBackStack()
+            }
+            true
         }
     }
 }
